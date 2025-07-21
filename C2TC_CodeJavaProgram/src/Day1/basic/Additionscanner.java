23380c0ffd634a27759dbc6df493ec302c78bca1
aravi <aravi@DESKTOP-NVUEGA2>
2025-07-21 19:03:48 +0530

    my day1 project

---
+++ C2TC_CodeJavaProgram/src/Day1/basic/Additionscanner.java
@@ -0,0 +1,17 @@
+package Day1.basic;
+
+import java.util.Scanner;
+public class Additionscanner {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Values:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
---
+++ C2TC_CodeJavaProgram/src/Day1/basic/Mainsignature.java
@@ -0,0 +1,17 @@
+package Day1.basic;
+
+public class Mainsignature {
+public static void main()
+{
+	System.out.println("Welcome from main");
+}
+ public static void main(char c[])
+{
+	System.out.println("Welcome from main 2");
+}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
